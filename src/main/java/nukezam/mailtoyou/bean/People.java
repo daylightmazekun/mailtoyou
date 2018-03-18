a52befd8ef7652e50085e1ac20f592f78623ed83
Daylightmazekun <ma.zekun@qq.com>
2018-03-18 19:39:16 +0800

    complete

---
+++ src/main/java/nukezam/mailtoyou/bean/People.java
@@ -0,0 +1,36 @@
+package nukezam.mailtoyou.bean;
+
+import java.util.List;
+
+/** 
+* @Package :nukezam.mailtoyou.bean
+* @Title: People.java 
+* @Package nukezam.mailtoyou.bean 
+* @author zekun ma burmaing@gmail.com   
+* @date 2018年1月10日 下午9:53:06 
+* @version V1.0   
+*/
+public class People {
+	private Integer id;
+	private String name;
+	private String email;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
